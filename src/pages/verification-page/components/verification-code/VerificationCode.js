018d8102833b0ff22c8e42e2de6790a91b8a88a3
madibarca <abzhanma@gmail.com>
2022-11-07 14:20:25 +0600

    Verification Code added

--- src/pages/verification-page/components/verification-code/VerificationCode.js
+++ src/pages/verification-page/components/verification-code/VerificationCode.js
@@ -1,21 +1,27 @@
 import React from "react";
 
+import ReactCodeInput from "react-code-input";
+import { VerificationCodeInputStyles } from "./VerificationCodeInputStyles";
+
+import "./VerificationCode.css";
+
 const VerificationCode = () => {
+
+
   return (
     <div className="verificationCode">
-      <h3>We’ve sent you a verification code at you@company.com</h3>
+      <h3>We’ve sent you a verification code at <span>you@company.com</span></h3>
 
       <p>Verification code</p>
-      <div className="verificationCodePinBox">
-        <input
-          className="verificationCodePinEntry"
-          type="number"
-          maxLength="4"
-          autoComplete="off"
-        />
-      </div>
+      <ReactCodeInput type="text" fields={4} {...VerificationCodeInputStyles}/>
+       
+      <button>Submit</button>
+      
+      <p className="verificationCodeResendEmail">Didn’t recieve a code? <span>Resend email</span></p>
     </div>
   );
 };
 
+
 export default VerificationCode;
+
---
+++ src/pages/verification-page/components/verification-code/VerificationCodeInputStyles.js
@@ -0,0 +1,45 @@
+export const VerificationCodeInputStyles = {
+    inputStyle: {    
+       marginTop: 0,
+       marginBottom: 0,
+  
+      marginRight:  '8px',
+      marginLeft: 0,
+      MozAppearance: 'textfield',
+      width: '62px',
+      height: '64px',
+      borderRadius: '8px',
+
+      fontStyle: "normal",
+      fontWeight: 300,
+      fontSize: "48px",
+      lineHeight: "60px",
+      textAlign: "center",
+      letterSpacing: "-0.02em",
+      color: "rgba(26, 28, 28, 0.4)",
+
+
+      outlineColor: '#009FD6',
+      border: '1px solid #E4E4E3'
+    },
+    inputStyleInvalid: {
+        marginTop: 0,
+        marginBottom: 0,
+      margin:  '8px',
+      MozAppearance: 'textfield',
+      width: '62px',
+      height: '64px',
+      borderRadius: '8px',
+
+      fontStyle: "normal",
+      fontWeight: 300,
+      fontSize: "48px",
+      lineHeight: "60px",
+      textAlign: "center",
+      letterSpacing: "-0.02em",
+      color: "rgba(26, 28, 28, 0.4)",
+
+      outlineColor: 'red',
+      border: '1px solid #E4E4E3'
+    }
+  }
