06f95e4e703fb9cfa6ed7faada3dcc309081d248
Virenst <islam.virenst@gmail.com>
2022-11-07 13:07:29 +0600

    verification img and title, routing for verification page

--- src/pages/verification-page/components/verification-title/VerificationTitle.js
+++ src/pages/verification-page/components/verification-title/VerificationTitle.js
@@ -1,7 +1,21 @@
+import arrowLeft from "../../../../assets/verification-page/verification-title/arrowLeft.png";
+
 import "./VerificationTitle.css";
 
 const VerificationTitle = () => {
-  return <div></div>;
+  return (
+    <div className="verificationTitleWrapper">
+      <div>
+        <img src={arrowLeft} alt="arrow" className="verificationTitleImg" />
+        <span className="verificationTitleBack">Back</span>
+      </div>
+      <h1>Verify your eligibility for the Finisher NFT</h1>
+      <p>
+        The Finisher NFT can only be minted by the finishers of
+        <span>Ironman Israel 2022</span> held on 25th November 2022.
+      </p>
+    </div>
+  );
 };
 
 export default VerificationTitle;
