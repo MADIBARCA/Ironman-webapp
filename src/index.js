8346465ccc8d7e7b81cb6958521f8904b05a9da4
madibarca <abzhanma@gmail.com>
2022-11-07 15:45:13 +0600

    Fonts changed

--- src/index.js
+++ src/index.js
@@ -8,9 +8,12 @@ import "./index.css";
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <BrowserRouter>
-<link rel="preconnect" href="https://fonts.googleapis.com"/>
-<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin/>
-<link href="https://fonts.googleapis.com/css2?family=Archivo+Black&family=Archivo:wght@400&display=swap" rel="stylesheet"/>
+    <link rel="preconnect" href="https://fonts.googleapis.com" />
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
+    <link
+      href="https://fonts.googleapis.com/css2?family=Archivo+Black&family=Archivo:wght@500&display=swap"
+      rel="stylesheet"
+    />
     <App />
   </BrowserRouter>
 );
