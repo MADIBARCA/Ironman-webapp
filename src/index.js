200d3047016f496413fc709bdc0ad838a0725d06
madibarca <abzhanma@gmail.com>
2022-11-07 15:43:04 +0600

    Fonts added

--- src/index.js
+++ src/index.js
@@ -8,6 +8,9 @@ import "./index.css";
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <BrowserRouter>
+<link rel="preconnect" href="https://fonts.googleapis.com"/>
+<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin/>
+<link href="https://fonts.googleapis.com/css2?family=Archivo+Black&family=Archivo:wght@400&display=swap" rel="stylesheet"/>
     <App />
   </BrowserRouter>
 );
