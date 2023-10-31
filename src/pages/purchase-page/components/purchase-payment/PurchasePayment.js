1a74925c1f6906eafbd5da9e24577bd965646fb5
madibarca <abzhanma@gmail.com>
2023-10-31 19:25:03 +0600

    verification-code added

--- src/pages/purchase-page/components/purchase-payment/PurchasePayment.js
+++ src/pages/purchase-page/components/purchase-payment/PurchasePayment.js
@@ -26,7 +26,7 @@ const PurchasePayment = ({setPurchaseType}) => {
       <p style={{margin: 0, marginBottom: '8px', marginTop: "24px"}}>Have a coupon code?</p>
       <div className="purchasePaymentInputBox">
         <input placeholder="Enter coupon code"/>
-        <button>Apply</button>
+        <button>Apply</button> 
       </div>
 
       <ProceedButton text={"Purchase NFT"} onClick={()=>setPurchaseType("success")}/>
